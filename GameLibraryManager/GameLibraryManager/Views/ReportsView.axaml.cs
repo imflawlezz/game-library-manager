1d960cd7a4253a1bad7e36985d34d672f5c4405f
imflawlezz <egorart6@gmail.com>
2025-12-30 18:39:17 +0100

    feat: add reports view and management dialog for user report handling, including feedback submission and review functionality

---
+++ GameLibraryManager/GameLibraryManager/Views/ReportsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace GameLibraryManager.Views
+{
+    public partial class ReportsView : UserControl
+    {
+        public ReportsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
---
+++ GameLibraryManager/GameLibraryManager/Views/ReviewReportDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace GameLibraryManager.Views
+{
+    public partial class ReviewReportDialog : Window
+    {
+        public ReviewReportDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
